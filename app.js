445249458affc8522ee9cae6333da913dd667dcd
binu <binu@moovooz.com>
2016-05-27 18:25:04 +0530

    bootstrap boilerplate + ajax & ejs

--- app.js
+++ app.js
@@ -6,10 +6,13 @@
 
     var express = require('express');           // Require express
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/mean');
 
+    // DB connection
+    mongoose.connect('mongodb://localhost/mean');
     var Schema = mongoose.Schema;
 
+
+    // Movie Schema
     var movieSchema = new Schema({
         "name": {
             type: String,
@@ -17,29 +20,34 @@
         }
     });
 
+    // Movie Model
+    var movieModel = mongoose.model('mine', movieSchema)
 
 
-    var movieModel = mongoose.model('mine',movieSchema)
-
-    //var movie = new movieModel({'name':"binoop"});
-    //movie.save();
-
-    movieModel.find({},function(err,movies){
-       console.log(err);
-       console.log(movies);
+    movieModel.find({}, function (err, movies) {
+        console.log(err);
+        console.log(movies);
     })
 
     // Create App
     var routes = express();
 
+    // view engine
+    routes.set('view engine', 'ejs');
+
     // Home Routes
     routes.get('/', function (request, response) {
-        response.send('Hello World!');
+        response.render('movie/index', {user: 'name Binoop'});
     });
 
-    routes.get(/a/, function (request, response) {
-        response.send('Hello sWorld!');
+    routes.post('/test-data', function (request, response) {
+        var jsonUsers = [{user: 'my pseudonym Montae'},{user: 'who ever i want to be'},{user: 'sometime the dark knight'},{user: 'am a villian or an hero'}];
+        response.send(jsonUsers[[Math.floor(Math.random()* jsonUsers.length)]]);
     });
+
+    //routes.get(/a/, function (request, response) {
+    //    response.send('Hello sWorld!');
+    //});
 
     // Set up to port to listen.
     routes.listen(2000, function () {
@@ -52,6 +60,8 @@
      * eg : param1 = '/static' ==>  http://localhost:2000/static/public/js/start.js
      * eg : no value passed ==> http://localhost:2000/public/js/start.js
      */
-    routes.use('/static', express.static('public'));
+    routes.use(express.static('public'));
+    routes.use('/node',express.static('node_modules'));
+    routes.use('/views',express.static('views'));
 })();
 
